9b93ced1abe2486854c9ec533fd590df11cacd9e
Othmansalahi <ootmansalhi.24@gmail.com>
2024-05-05 16:27:23 +0100

    Refactor pagination functionality in Server class

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
-
+""" module doc """
 import csv
 import math
 from typing import List, Tuple
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """func doc"""
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -25,10 +25,17 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-        start, end = index_range(page, page_size)
+        """Get page from the dataset"""
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        start, end = self.index_range(page, page_size)
         return self.dataset()[start:end]
+
+    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
+        """function doc"""
+        startPage = (page - 1) * page_size
+        endPage = page * page_size
+        return (startPage, endPage)
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         """function doc"""
@@ -42,12 +49,3 @@ class Server:
             "prev_page": page - 1 if page > 1 else None,
             "total_pages": total_pages,
         }
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """
-    Return a tuple of size two containing a start index and an end index
-    """
-    start_page = (page - 1) * page_size
-    end_page = page * page_size
-    return (start_page, end_page)
